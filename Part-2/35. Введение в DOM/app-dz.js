ebfcde151704af2413f6a192679748996a61aeb4
rodionzam <rodionzam@yandex.ru>
2022-04-25 00:58:44 +0300

    JS - Part 2 / Lesson 35 edited and 35 DZ

---
+++ Part-2/35. Введение в DOM/app-dz.js
@@ -0,0 +1,124 @@
+/*
+* Задание 1
+*
+* Дана разметка.
+*
+* <!doctype html>
+* <html lang="en">
+* <head>
+*     <meta charset="UTF-8">
+*     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+*     <title>Document</title>
+* </head>
+* <body>
+*     <div>
+*         <p>Text</p>
+*         <p>Other</p>
+*         <p>Next</p>
+*         <p>Last</p>
+*     </div>
+*     <div></div>
+*     <div></div>
+*
+*     <script src="app-dz.js"></script>
+* </body>
+* </html>
+*
+* Зная структуру html, с помощью изученных методов получить (в консоль):
+*
+* 1. head
+* 2. body
+* 3. все дочерние элементы body и вывести их в консоль.
+* 4. первый div и все его дочерние узлы
+* а) вывести все дочерние узлы в консоль
+* б) вывести в консоль все дочерние узлы, кроме первого и последнего
+*
+* Для навигации по DOM использовать методы, которые возвращают только элементы
+*/
+
+console.log(document.head); // 1
+console.log(document.body); // 2
+console.log(document.body.children); // 3
+
+// 4-а
+const childNodes = document.querySelector('div').childNodes;
+console.log(childNodes);
+
+// 4-б
+const arrChildNodes = [];
+for (let i = 1; i < childNodes.length - 1; ++i) {
+    arrChildNodes.push(childNodes[i]);
+}
+console.log(arrChildNodes);
+
+/*
+* Задание 2
+*
+* Дана разметка.
+*
+* <!doctype html>
+* <html lang="en">
+* <head>
+*     <meta charset="UTF-8">
+*     <meta name="viewport"
+*           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+*     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+*     <title>Hello world</title>
+* </head>
+* <body>
+*     <div>
+*         <article>
+*             <p>Lorem ipsum dolor sit amet, odio omnesque ius cu, quo ex atqui antiopam. At detracto menandri eos. Duo in causae viderer, graeci <a href="#">reprehendunt</a> has in. Decore <mark>nemore</mark> philosophia te pro, nobis legere causae ex mei, odio putant mentitum ea ius. Vix nostro deserunt explicari eu.</p>
+*         </article>
+*     </div>
+*     <ul>
+*         <li><a href="#">Link1</a></li>
+*         <li><a href="#">Link2</a></li>
+*         <li><a href="#">Link3</a></li>
+*         <li><a href="#">Link4</a></li>
+*     </ul>
+*     <span></span>
+*     <a href="#">Some link</a>
+*
+*     <script src="app-dz.js"></script>
+* </body>
+* </html>
+*
+* Создать функцию, которая принимает два элемента. Функция проверяет, является ли первый элемент родителем для второго:
+*
+* isParent(parent, child);
+*
+* isParent(document.body.children[0], document.querySelector('mark'));
+* // true так как первый див является родительским элементом для mark
+*
+* isParent(document.querySelector('ul'), document.querySelector('mark'));
+* // false так ul НЕ является родительским элементом для mark
+*
+* Функция принимает только DOM объекты. Обязательно проверяйте это.
+*/
+
+function isParent(parent, child) {
+
+}
+
+console.log(isParent(document.body.children[0], document.querySelector('mark')));
+console.log(isParent(document.querySelector('ul'), document.querySelector('mark')));
+
+/*
+* Задание 3
+*
+* Используя разметку из предыдущего задания.
+*
+* Получить список всех ссылок, которые не находятся внутри списка ul.
+*/
+
+
+
+/*
+* Задание 4
+*
+* Используя разметку из предыдущего задания.
+*
+* Найти элемент, который находится перед и после списка ul.
+*/
+
--- Part-2/35. Введение в DOM/app.js
+++ Part-2/35. Введение в DOM/app.js
@@ -11,3 +11,19 @@ console.log([...titles]);
 
 const h1 = document.getElementsByTagName('h1');
 console.log(h1); // HTMLCollection(2) [h1, h1] - HTMLCollection это живые коллекции, которые поменяются если изменится dom элемент
+
+// Пример свойств
+console.log(div.nextSibling); // Вернёт следующий узел после дива - #text
+console.log(div.nextElementSibling); // Вернёт следующий элемент после дива - <script src="app.js"></script>
+console.log(div.firstChild); // Первый дочерний узел - Lorem ipsum
+console.log(div.firstElementChild); // Первый дочерний элемент - <a href="" class="link">dolor</a>
+console.log(document.body.firstChild.nextSibling); // <!-- my comment -->
+
+console.log(document.body); // Получим тело нашего документа
+console.log(document.links); // Получим все ссылки в нашем документе
+console.log(document.forms); // Получим все формы в нашем документе
+
+// Объектная модель браузера
+console.log(window);
+console.log(navigator);
+console.log(location);
