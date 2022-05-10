f6a203a7b01667a3b97a6f815abb462294079584
rodionzam <rodionzam@yandex.ru>
2022-05-10 20:52:39 +0300

    JS - Part 2 / Lesson 38 and 39 and DZ 2/3

--- Part-2/39. Всплытие и погружение события/app-dz-2.js
+++ Part-2/39. Всплытие и погружение события/app-dz-2.js
@@ -1,33 +1,21 @@
 /*
 * Задание 1
 *
-*
+* Реализовать примитивный дропдаун. Изначально все dropdown-menu скрыты через класс .d-none. При клике на dropdown-item должен отображаться блок dropdown-menu
+* который вложен именно в тот dropdown-item на котором произошел клик. При повторном клике на этот же dropdown-item блок dropdown-menu должен закрыться.
+* При клике на любой другой dropdown-item уже открытый dropdown-menu должен закрываться а на тот который кликнули открываться.
 */
 
+const [...dropItem] = document.querySelectorAll('.dropdown-item');
 
+dropItem.forEach(itemDrop => {
+    itemDrop.addEventListener('click', e => {
+        itemDrop.querySelector('.dropdown-menu').classList.toggle('d-none');
 
-
-/*
-* Задание 2
-*
-*
-*/
-
-
-
-
-/*
-* Задание 3
-*
-*
-*/
-
-
-
-
-/*
-* Задание 4
-*
-*
-*/
-
+        dropItem.forEach(itemHide => {
+            if (itemDrop !== itemHide) {
+                itemHide.querySelector('.dropdown-menu').classList.add('d-none');
+            }
+        });
+    });
+});
--- Part-2/39. Всплытие и погружение события/Решение преподавателя 2.js
+++ Part-2/39. Всплытие и погружение события/Решение преподавателя 2.js
@@ -1,13 +1,21 @@
 //1
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+// В данной переменной мы будем хранить текущее открытое меню
+let currentOpenedMenu = null;
 
+function toggleDropdownMenu(e) {
+    // получаем блок меню внутри .dropdown-item
+    const menu = e.currentTarget.querySelector('.dropdown-menu');
+    // метод toggle возвращает булевое значение, если клас удален то вернет false а если добавлен то true. Это значение мы сохраняем в переменную
+    const isAdded = menu.classList.toggle('d-none');
+    // если в currentOpenedMenu уже есть блок и он не равен текущем menu то мы его скрываем
+    if (currentOpenedMenu && currentOpenedMenu !== menu) {
+        currentOpenedMenu.classList.add('d-none');
+    }
+    // Если класс d-none был удален то значет меню видимо и мы его сохраняем в переменную currentOpenedMenu
+    if (!isAdded) {
+        currentOpenedMenu = menu;
+    }
+}
 
-
-//2
-
-
-
-//3
-
-
-
-//4
+dropdownItems.forEach(d => d.addEventListener('click', toggleDropdownMenu));
