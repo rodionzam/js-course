a20e7ba2a22ed126b61aee4111083a04fc128dc0
Rodion <rodionzam@yandex.ru>
2022-07-04 17:25:10 +0300

    JS - Part 3 / Lesson 47 and 48

---
+++ Part-3/48. Введение в Ajax/app.js
@@ -0,0 +1,44 @@
+const btn = document.querySelector('button');
+const container = document.querySelector('.j-container-posts');
+
+function getPost(cb) {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+    xhr.addEventListener("load", () => {
+        const response = JSON.parse(xhr.responseText);
+        cb(response);
+    });
+
+    xhr.addEventListener("error", () => {
+        console.log('error');
+    });
+
+    xhr.send();
+}
+
+function renderPosts(response) {
+    const fragment = document.createDocumentFragment();
+
+    response.forEach(post => {
+        const card = document.createElement('div');
+        card.classList.add('card', 'mt-3');
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        const title = document.createElement('h5');
+        title.classList.add('card-title');
+        title.textContent = post.title;
+        const article = document.createElement('p');
+        article.classList.add('card-text');
+        article.textContent = post.body;
+        cardBody.appendChild(title);
+        cardBody.appendChild(article);
+        card.appendChild(cardBody);
+        fragment.appendChild(card);
+    });
+
+    container.appendChild(fragment);
+}
+
+btn.addEventListener('click', e => {
+    getPost(renderPosts);
+});
