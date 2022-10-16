98a24ca50a91e6b662f19a862a9da4b883015598
rodionzam <rodionzam@yandex.ru>
2022-10-16 23:18:44 +0300

    JS - Part 7 / Lesson 80-82

---
+++ Part-7/80-86. Проект Login/src/js/app.js
@@ -0,0 +1,37 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import '../css/style.css';
+
+import UI from "./config/ui.config";
+import {validate} from "./helpers/validate";
+import {showInputError, removeInputError} from "./views/form";
+import {login} from "./services/auth.service";
+
+const {form, inputEmail, inputPassword} = UI;
+const inputs = [inputEmail, inputPassword];
+
+// Events
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    onSubmit();
+});
+inputs.forEach(el => el.addEventListener('focus', () => removeInputError(el)));
+
+// Handlers
+async function onSubmit() {
+    const isValidForm = inputs.every((el) => {
+        const isValidateInput = validate(el);
+        if (!isValidateInput) {
+            showInputError(el);
+        }
+        return isValidateInput;
+    });
+
+    if (!isValidForm) return;
+
+    try {
+        await login(inputEmail.value, inputPassword.value);
+        // show success notify
+    } catch (err) {
+        // show error notify
+    }
+}
